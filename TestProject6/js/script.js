faf5f794895cd43cf0442acaf4efb9f64683a685
MaxyFrontend <63448370+MaxyFrontend@users.noreply.github.com>
2020-11-16 09:43:52 +0200

    Update script.js

--- TestProject6/js/script.js
+++ TestProject6/js/script.js
@@ -1,10 +1,39 @@
 let nav = document.querySelector('.nav');
 let navButton = document.querySelector('.nav-button');
 let navButton_lines = document.querySelectorAll('.nav-button__lines');
+let headerLayer = document.querySelector('.header-layer');
+let navItems = document.querySelectorAll('.nav-items');
 navButton.onclick = function() {
 	nav.classList.toggle('nav-active'),
+	headerLayer.classList.toggle('header-layer-active'),
 	navButton.classList.toggle('nav-button-active');
 	for( let i = 0; i < navButton_lines.length; i++){
 		navButton_lines[i].classList.toggle('nav-button__lines-active');
 	}
 };
+function removeClasses() {
+	nav.classList.remove('nav-active'),
+	headerLayer.classList.remove('header-layer-active'),
+	navButton.classList.remove('nav-button-active');
+	for( let i = 0; i < navButton_lines.length; i++){
+		navButton_lines[i].classList.remove('nav-button__lines-active');
+	}
+}
+headerLayer.onclick = removeClasses;
+ for( let i = 0; i < navItems.length; i++){
+		navItems[i].onclick = removeClasses;
+	}
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
